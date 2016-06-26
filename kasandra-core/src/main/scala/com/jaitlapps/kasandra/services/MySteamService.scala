8952ff7e262ec63cbff650a7df1bf75d056b25bc
Jaitl <jaitl@outlook.com>
2016-06-26 23:12:46 +0300

    MySteam fix

--- kasandra-core/src/main/scala/com/jaitlapps/kasandra/services/MySteamService.scala
+++ kasandra-core/src/main/scala/com/jaitlapps/kasandra/services/MySteamService.scala
@@ -3,8 +3,14 @@ package com.jaitlapps.kasandra.services
 import java.io.File
 
 import com.jaitlapps.kasandra.configs.MySteamConfig
-import ru.stachek66.nlp.mystem.holding.Factory
+import ru.stachek66.nlp.mystem.holding.{Request, Response, Factory}
+import ru.stachek66.nlp.mystem.model.Info
 
 class MySteamService(config: MySteamConfig) {
-  val mystemAnalyzer = new Factory(config.arguments).newMyStem(config.version, Option(new File(config.path))).get
+  private val mystemAnalyzer = new Factory(config.arguments)
+    .newMyStem(config.version, Option(new File(config.path))).get
+
+  def steam(text: String): Seq[Info] = {
+    mystemAnalyzer.analyze(Request(text)).info.toSeq
+  }
 }
