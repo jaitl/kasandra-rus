64b97e6313c97e1793b457e58c364a62752e0e83
Jaitl <jaitl@outlook.com>
2017-02-13 23:34:47 +0300

    fixes

--- kasandra_scrapy/kasandra_scrapy/parsers/vk_wall_parser.py
+++ kasandra_scrapy/kasandra_scrapy/parsers/vk_wall_parser.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+import json
+
+
 class VkWallParser(object):
-    def parse(self, json):
+    def parse(self, json_data):
+        items = data['response']['items']
+        print(data.decode("utf-8"))
         return ""
