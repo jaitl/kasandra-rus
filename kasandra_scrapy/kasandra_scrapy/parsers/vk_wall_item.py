48283a91b16c6d52d8580434bf3d1334aa173573
Jaitl <jaitl@outlook.com>
2017-02-15 16:58:36 +0300

    complete vk_wall_parser

---
+++ kasandra_scrapy/kasandra_scrapy/parsers/vk_wall_item.py
@@ -0,0 +1,4 @@
+class VkWallItem:
+    def __init__(self, url, date):
+        self.url = url
+        self.date = date
--- kasandra_scrapy/kasandra_scrapy/parsers/vk_wall_parser.py
+++ kasandra_scrapy/kasandra_scrapy/parsers/vk_wall_parser.py
@@ -1,9 +1,34 @@
 # -*- coding: utf-8 -*-
 import json
+import re
+from .vk_wall_item import VkWallItem
 
 
 class VkWallParser(object):
+    def __init__(self):
+        self.__regex = "(https?://[^\\s/$.?#].[^\\s]*)"
+
     def parse(self, json_data):
+        data = json.loads(json_data)
         items = data['response']['items']
-        print(data.decode("utf-8"))
-        return ""
+
+        result = []
+
+        for item in items:
+            date = item['date']
+            text = item['text']
+
+            urls = set(re.findall(self.__regex, text))
+
+            try:
+                for attachment in item['attachments']:
+                    if attachment['type'] == 'link':
+                        urls.add(attachment['link']['url'])
+            except:
+                pass
+
+            for url in urls:
+                wall_item = VkWallItem(url, date)
+                result.append(wall_item)
+
+        return result
