ab00885bad1d158578ec76afe3df510c16e9fd9a
Jaitl <jaitl@outlook.com>
2017-02-15 19:10:58 +0300

    vk-wall config file

--- kasandra_scrapy/kasandra_scrapy/spiders/vk_wall.py
+++ kasandra_scrapy/kasandra_scrapy/spiders/vk_wall.py
@@ -2,6 +2,8 @@
 import scrapy
 import json
 from furl import furl
+from yaml import load
+import os
 from ..parsers import vk_wall_parser
 
 
@@ -11,11 +13,14 @@ class VkWallParser(scrapy.Spider):
     api_url = 'https://api.vk.com/method/wall.get'
 
     def start_requests(self):
+        configs = self.load_config("configs/vk-wall.yaml")
+        print(configs)
         params = {
             "v": "5.53",
             "domain": "test",
             "count": 100,
-            "offset": 0
+            "offset": 0,
+            "filter": "owner"
         }
 
         yield self.create_request(params, self.parse)
@@ -41,3 +46,10 @@ class VkWallParser(scrapy.Spider):
         url = furl(self.api_url).add(params).url
         request = scrapy.Request(url=url, callback=callback, meta={'params': params})
         return request
+
+    def load_config(self, path):
+        work_dir = os.path.dirname(__file__)
+        filename = os.path.join(work_dir, path)
+        with open(filename, 'r') as stream:
+            configs = load(stream)
+        return configs
