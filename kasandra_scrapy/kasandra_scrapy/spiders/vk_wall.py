300a0078367d01aa9e7263771943d9196de0faae
Jaitl <jaitl@outlook.com>
2017-02-15 17:39:15 +0300

    complete vk wall parser

--- kasandra_scrapy/kasandra_scrapy/spiders/vk_wall.py
+++ kasandra_scrapy/kasandra_scrapy/spiders/vk_wall.py
@@ -1,26 +1,43 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import json
 from furl import furl
-from parsers import vk_wall_parser
+from ..parsers import vk_wall_parser
 
 
 class VkWallParser(scrapy.Spider):
     name = "wall"
+    wall_max_count = 100
+    api_url = 'https://api.vk.com/method/wall.get'
 
     def start_requests(self):
-        api_url = 'https://api.vk.com/method/wall.get'
         params = {
             "v": "5.53",
-            "domain": "tj",
-            "count": 3,
+            "domain": "test",
+            "count": 100,
             "offset": 0
         }
 
-        url = furl(api_url).add(params).url
-
-        yield scrapy.Request(url=url, callback=self.parse)
+        yield self.create_request(params, self.parse)
 
     def parse(self, response):
-        parser = vk_wall_parser.VkWallParser
+        params = dict(response.meta['params'])
+        parser = vk_wall_parser.VkWallParser()
         data = response.text
-        wall_raw = parser.parse(data)
+        wall_items = parser.parse(data)
+
+        count_items = self.count_items(data)
+
+        if count_items == self.wall_max_count:
+            params['offset'] += self.wall_max_count
+            yield self.create_request(params, self.parse)
+
+    def count_items(self, json_data):
+        data = json.loads(json_data)
+        items = data['response']['items']
+        return len(items)
+
+    def create_request(self, params, callback):
+        url = furl(self.api_url).add(params).url
+        request = scrapy.Request(url=url, callback=callback, meta={'params': params})
+        return request
