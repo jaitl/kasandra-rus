01805f37eeee7341202cc80b1a1d7827105185d9
Jaitl <jaitl@outlook.com>
2017-04-02 15:58:19 +0300

    add move sites

--- kasandra-crawler/src/main/scala/com/jaitlapps/kasandra/crawler/models/SiteType.scala
+++ kasandra-crawler/src/main/scala/com/jaitlapps/kasandra/crawler/models/SiteType.scala
@@ -6,8 +6,24 @@ object SiteType {
   def apply(name: String) : SiteType = name match {
     case RtSite.name => RtSite
     case RiaSite.name => RiaSite
+    case LifeSite.name => LifeSite
+    case OneTvSite.name => OneTvSite
+    case LentaSite.name => LentaSite
+    case RbcSite.name => RbcSite
+    case KpSite.name => KpSite
+    case VestiSite.name => VestiSite
+    case GazetaSite.name => GazetaSite
+    case RePublickSite.name => RePublickSite
   }
 
   case object RtSite extends SiteType("RT")
   case object RiaSite extends SiteType("RIA")
+  case object LifeSite extends SiteType("LIFE")
+  case object OneTvSite extends SiteType("1TV")
+  case object LentaSite extends SiteType("LENTA")
+  case object RbcSite extends SiteType("RBC")
+  case object KpSite extends SiteType("KP")
+  case object VestiSite extends SiteType("VESTI")
+  case object GazetaSite extends SiteType("GAZETA")
+  case object RePublickSite extends SiteType("REPUBLICK")
 }
